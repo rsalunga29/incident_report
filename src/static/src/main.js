72edac67d18fe92d5c2819d505b3e36bbfd4a9b2
Akio <rsalunga29@gmail.com>
2021-01-08 16:10:37 +0800

    buefy

--- src/static/src/main.js
+++ src/static/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 import router from './router'
 import store from './store'
@@ -11,6 +13,7 @@ Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(Buefy)
 
 new Vue({
   router,
