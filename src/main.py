d44232ea084bb36b60617aad91bec9aafb5bf5f7
Akio <rsalunga29@gmail.com>
2021-01-11 20:33:13 +0800

    changed fastapi config to for 'api' prefix in url

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from starlette.applications import Starlette
 
 from .api import api_router
 
-app = FastAPI()
+# we create the ASGI for the app
+app = Starlette()
 
-app.include_router(api_router, prefix='/v1')
+# we create the ASGI for the frontend
+frontend = Starlette()
+
+# we create the Web API framework
+api = FastAPI()
+
+api.include_router(api_router, prefix='/v1')
+
+app.mount('/api', app=api)
+app.mount('/', app=frontend)
