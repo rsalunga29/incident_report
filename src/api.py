c93b2e840a7ea796e430943a969eae01efe9be8e
Akio <rsalunga29@gmail.com>
2021-01-27 23:59:40 +0800

    add issues router

--- src/api.py
+++ src/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 
 from src.auth.views import auth_router, user_router
+from src.issues.views import issues_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix='/auth', tags=['auth'])
 api_router.include_router(user_router, prefix='/users', tags=['users'])
+api_router.include_router(issues_router, prefix='/issues', tags=['issues'])
